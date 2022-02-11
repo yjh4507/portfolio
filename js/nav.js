e0e667a27707814152d8b2b40e29fb1fb6a31955
yjh4507 <yyjh4507@naver.com>
2022-02-11 17:56:19 +0900

    2022-02-11

---
+++ js/nav.js
@@ -0,0 +1,16 @@
+$(function () {
+    $(".btm_box_1").fadeIn();
+    $('.btm_nav_ul>li:nth-of-type(1)').click(function () {
+      $('li').removeClass("active");
+      $(this).addClass("active");
+      $(".btm_box_1").show();
+      $(".btm_box_2").hide();
+    });
+    $('.btm_nav_ul>li:nth-of-type(2)').click(function () {
+      $('li').removeClass("active");
+      $(this).addClass("active");
+      $(".btm_box_1").hide();
+      $(".btm_box_2").show();
+    });
+  });
+  
