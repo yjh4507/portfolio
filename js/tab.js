60029add092a494323383f74b1c10e9ae4f9fc94
yjh4507 <93234827+yjh4507@users.noreply.github.com>
2022-02-14 13:09:35 +0900

    2022-02-14

---
+++ js/tab.js
@@ -0,0 +1,43 @@
+$(function () {
+  $(".tab_1").click(function(){
+    $(".dim, .contents1").fadeIn();
+  });
+  $(".dim").click(function(){
+    $(".dim, .contents1").fadeOut();
+  });
+
+  $(".tab_2").click(function(){
+    $(".dim, .contents2").fadeIn();
+  });
+  $(".dim").click(function(){
+    $(".dim, .contents2").fadeOut();
+  });
+
+  $(".tab_3").click(function(){
+    $(".dim, .contents3").fadeIn();
+  });
+  $(".dim").click(function(){
+    $(".dim, .contents3").fadeOut();
+  });
+
+  $(".tab_4").click(function(){
+    $(".dim, .contents4").fadeIn();
+  });
+  $(".dim").click(function(){
+    $(".dim, .contents4").fadeOut();
+  });
+
+  $(".tab_5").click(function(){
+    $(".dim, .contents5").fadeIn();
+  });
+  $(".dim").click(function(){
+    $(".dim, .contents5").fadeOut();
+  });
+
+  $(".tab_6").click(function(){
+    $(".dim, .contents6").fadeIn();
+  });
+  $(".dim").click(function(){
+    $(".dim, .contents6").fadeOut();
+  });
+});
